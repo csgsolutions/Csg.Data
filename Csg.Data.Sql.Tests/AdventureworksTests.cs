5e894cd4fa00edc37997bbac7d3b413cfdca79df
Justin Buchanan <buchanan@csgsolutions.com>
2019-09-18 12:19:12 -0500

    Remove database tests until we get this running.

--- Csg.Data.Sql.Tests/AdventureworksTests.cs
+++ Csg.Data.Sql.Tests/AdventureworksTests.cs
@@ -17,29 +17,29 @@ namespace TestProject
     [TestClass]
     public class AdventureworksTests
     {
-        private const string _connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=AdventureWorks;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //private const string _connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=AdventureWorks;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-        private static System.Data.SqlClient.SqlConnection CreateConnection()
-        {
-            var c = new System.Data.SqlClient.SqlConnection(_connectionString);
-            c.Open();
-            return c;
-        }
+        //private static System.Data.SqlClient.SqlConnection CreateConnection()
+        //{
+        //    var c = new System.Data.SqlClient.SqlConnection(_connectionString);
+        //    c.Open();
+        //    return c;
+        //}
 
-        [TestMethod]
-        public void Test_Person_SelectByType()
-        {
-            using (var conn = CreateConnection())
-            {
-                conn.QueryBuilder("Person.Person")
-                    .Select("BusinessEntityID", "FirstName", "LastName")
-                    .Where(x => x.FieldEquals("PersonType", "EM", System.Data.DbType.StringFixedLength, size: 2))
-                    .OrderBy("LastName")
-                    .OrderByDescending("FirstName")
-                    .ExecuteReader();
+        //[TestMethod]
+        //public void Test_Person_SelectByType()
+        //{
+        //    using (var conn = CreateConnection())
+        //    {
+        //        conn.QueryBuilder("Person.Person")
+        //            .Select("BusinessEntityID", "FirstName", "LastName")
+        //            .Where(x => x.FieldEquals("PersonType", "EM", System.Data.DbType.StringFixedLength, size: 2))
+        //            .OrderBy("LastName")
+        //            .OrderByDescending("FirstName")
+        //            .ExecuteReader();
 
-            }
-        }
+        //    }
+        //}
 
        
     }
