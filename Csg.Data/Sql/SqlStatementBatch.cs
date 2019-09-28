0ce9b38ebf29b25b9f2bc145f5101e8adc246890
Justin Buchanan <buchanan@csgsolutions.com>
2019-09-27 22:53:08 -0500

    Add new return type for batch statments.

---
+++ Csg.Data/Sql/SqlStatementBatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csg.Data.Sql
+{
+    /// <summary>
+    /// Represents a batch of multiple SQL statements combined into a single object.
+    /// </summary>
+    public class SqlStatementBatch : SqlStatement
+    {
+        /// <summary>
+        /// Initializes a new instance with the given statement count and command text.
+        /// </summary>
+        /// <param name="statementCount"></param>
+        /// <param name="commandText"></param>
+        /// <param name="parameters"></param>
+        public SqlStatementBatch(int statementCount, string commandText, ICollection<DbParameterValue> parameters) : base(commandText, parameters)
+        {
+            this.Count = statementCount;
+        }
+
+        /// <summary>
+        /// Gets the number of statements in the batch.
+        /// </summary>
+        public int Count { get; protected set; }
+    }
+}
